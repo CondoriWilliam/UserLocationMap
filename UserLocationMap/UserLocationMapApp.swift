82fff602743d115d4629a497c1d2472d4adf7e90
CondoriWilliam <wcondorian@unsa.edu.pe>
2024-11-11 10:37:25 -0800

    Initial Commit

---
+++ UserLocationMap/UserLocationMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationMapApp.swift
+//  UserLocationMap
+//
+//  Created by galaxy on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
